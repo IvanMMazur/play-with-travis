0b09db46fef8dd348792a46bd5f529548cf876ed
ivan <ivanmmazur@gmail.com>
2020-04-06 23:02:23 +0200

    checktest.py

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ class TestDevOps(unittest.TestCase):
     def test_conflict(self):
         self.assertEqual('Dev' + 'Ops', 'Conflict')
 
-if __name__ != '__main__':
+if __name__ == '__main__':
     unittest.main()
